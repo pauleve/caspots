4a8fb7d737297dcd5b4203af55cf29d7bf079a40
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2016-12-12 16:57:22 +0100

    setuptool

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 
 from setuptools import setup, find_packages
 
-version = "0.1"
+version = "0.3"
 
 setup(name="caspots",
     version = version,
     description = "Boolean network identification from time-series data",
     long_description = "",
     install_requires = [
-        "caspo",
+        "caspo>=3",
         #"pydot<=1.2",
     ],
     packages = find_packages(),
